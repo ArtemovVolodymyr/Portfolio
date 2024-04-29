6099e1c0cc3e060d3fde3478bbec0d70939979a5
ArtVovan <bogomol@gmail.com>
2024-04-29 23:13:35 +0300

    add a base structure of my portfolio

---
+++ proefolio_app/home_page/views.py
@@ -0,0 +1,8 @@
+# home_page.views
+
+from django.shortcuts import render
+
+
+def home_page(request):
+    return render(request, 'home_page/index_home.html')
+
---
+++ proefolio_app/proefolio_app/urls.py
@@ -0,0 +1,8 @@
+# proefolio_app.urls
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home_page.urls'))
+]
